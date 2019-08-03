3ac328bcf8ccb68f2b44ae1c5951a7a8e3e00581
azuline <azuline@riseup.net>
2019-08-03 15:26:27 -0400

    add apache-2.0 license

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,14 +1,11 @@
 import mock
-from click.testing import CliRunner
 
 from pixi.config import Config
 
 
 @mock.patch('pixi.config._validate_config')
 def test_singleton(validate_config):
-    with CliRunner().isolated_filesystem():
-        validate_config.return_value = True
-
-        config1 = Config()
-        config2 = Config()
-        assert config1 is config2
+    validate_config.return_value = True
+    config1 = Config()
+    config2 = Config()
+    assert config1 is config2
