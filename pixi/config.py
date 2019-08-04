2a326b2d9e0b13369028b277973248d7c2b66528
azuline <azuline@riseup.net>
2019-08-03 20:12:21 -0400

    change config to directly return configparser instead of itself

--- pixi/config.py
+++ pixi/config.py
@@ -1,3 +1,4 @@
+import os
 from configparser import ConfigParser
 
 from pixi import CONFIG_DIR
@@ -5,26 +6,22 @@ from pixi.errors import InvalidConfig
 
 _CONFIG_VALUES = {
     'download_directory': None,
-    'filename_format': None,
 }
 
 
 class Config:
     """
-    A singleton that returns the same instance when instantiated.
+    A singleton that returns the same ConfigParser instance when
+    instantiated.
     """
 
-    __instance = None
+    __config = None
 
     def __new__(cls):
-        if cls.__instance is None:
-            cls.__instance = object.__new__(cls)
-        return cls.__instance
-
-    def __init__(self):
-        self.config = _load_config()
-        if not _validate_config(self.config):
-            raise InvalidConfig
+        if cls.__config is None:
+            cls.__config = _load_config()
+            _validate_config(cls.__config)
+        return cls.__config
 
 
 def _load_config():
@@ -34,4 +31,14 @@ def _load_config():
 
 
 def _validate_config(config):
-    return False
+    if not config:
+        raise InvalidConfig('Config file is empty.')
+
+    try:
+        dl_dir = config['pixi']['download_directory']
+        if not os.path.isdir(dl_dir) or not os.access(dl_dir, os.W_OK):
+            raise InvalidConfig(
+                'Download directory does not exist or is not writeable.'
+            )
+    except KeyError:
+        raise InvalidConfig('Config file does not specify download directory.')
